ea1384f95fb3b5000049fdfe41269a5a1451d732
ImadEM21 <imad.elmahrad98@gmail.com>
2021-01-06 17:24:57 +0100

    end the auth posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const postController = require('../controllers/posts');
 
+const auth = require('../middlewares/auth');
 
-router.get('/posts', postController.getPosts);
-router.get('/posts/:id', postController.getPost);
-router.post('/posts', postController.createPost);
-router.put('/posts/:id', postController.modifyPost);
-router.delete('/posts/:id', postController.deletePost);
+
+router.get('/posts', auth, postController.getPosts);
+router.get('/posts/:id', auth, postController.getPost);
+router.post('/posts', auth, postController.createPost);
+router.put('/posts/:id', auth, postController.modifyPost);
+router.delete('/posts/:id', auth, postController.deletePost);
 
 
 module.exports = router;
