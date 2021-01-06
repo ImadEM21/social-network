10f37f9d7e850b4aee0e5b6642d8f9cb45529f47
ImadEM21 <imad.elmahrad98@gmail.com>
2021-01-06 16:46:28 +0100

    implement crud for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 exports.signup = (req, res) => {
     bcrypt.hash(req.body.password, 12)
     .then(hash => {
-        const user = new User({
+        const user =  new User({
             ...req.body,
             password: hash,
             userImage: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -39,5 +40,36 @@ exports.login = (req, res) => {
         })
         .catch(error => res.status(500).json({error}));
     })
+    .catch(error => res.status(500).json({error}));
+};
+
+exports.modifyUser = (req, res) => {
+    const user = req.file ? {
+        ...req.body,
+        userImage: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {
+        ...req.body
+    };
+    User.findByIdAndUpdate(req.params.id, {...user, _id: req.params.id})
+    .then(() => res.status(200).json({message: "Utilisateur bien modifié"}))
+    .catch(error => res.status(500).json({error}));
+};
+
+exports.deleteUser = (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        const filename = user.userImage.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            User.findByIdAndDelete(req.params.id)
+            .then(() => res.status(200).json({message: "Utilisateur bien supprimé"}))
+            .catch(error => res.status(500).json({error}));
+        });
+    })
+    .catch();
+};
+
+exports.getUser = (req, res) => {
+    User.findById(req.params.id)
+    .then(user => res.status(200).json({user}))
     .catch(error => res.status(500).json({error}));
 };
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const multer = require('../middlewares/multer-config');
 
 router.post('/signup', multer, userController.signup);
 router.post('/login', userController.login);
-
+router.get('/users/:id', userController.getUser);
+router.put('/users/:id', multer, userController.modifyUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
