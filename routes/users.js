544a7ea15d61da806775cea26d250f2b06e28aba
ImadEM21 <imad.elmahrad98@gmail.com>
2021-01-06 16:54:51 +0100

    add auth

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const userController = require('../controllers/users');
 
 const multer = require('../middlewares/multer-config');
+const auth = require('../middlewares/auth');
 
 router.post('/signup', multer, userController.signup);
 router.post('/login', userController.login);
 router.get('/users/:id', userController.getUser);
-router.put('/users/:id', multer, userController.modifyUser);
-router.delete('/users/:id', userController.deleteUser);
+router.put('/users/:id', auth, multer, userController.modifyUser);
+router.delete('/users/:id', auth, userController.deleteUser);
 
 module.exports = router;
